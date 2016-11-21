0bacc9a41eb7861ecf0868385ef0d8a379bc2020
Madison Fortenberry <fortenberry.rm@gmail.com>
2016-11-21 15:59:29 -0500

    Significant AStar performance increases: 1. Only 1 set container. 2. Maximum of 2 log(n) operations per neighbor. 3. Zero log(n) operations if neighbor's gScore is less than the tentative gScore. 4. Node is explicitly polymorphized: No v-table look ups.

--- AStar.hpp
+++ AStar.hpp
@@ -13,131 +13,109 @@
 
 namespace flabs
 {
-	template<class Node>
-	class AStar
+template<class Node>
+class AStar
+{
+public:
+
+	virtual ~AStar()
 	{
-		public:
-			typedef lessPointer<Node*> CompareNode;
+	}
 
-			virtual ~AStar()
-			{
-			}
-
-			struct CostNode
-			{
-				Node* node;
-				double fScore;
-
-				CostNode(Node* node, double cost) : node(node), fScore(cost)
-				{
-				}
-
-				bool operator<(const CostNode& other) const
-				{
-					if (fScore == other.fScore)
-						return node < other.node;
-					else
-						return fScore < other.fScore;
-				}
-			};
-
-			template<template<class, class, class...> class C, typename K,
-				typename V, typename... Args>
-			inline V GetWithDef(C<K, V, Args...>& m, K& key)
-			{
-				typename C<K, V, Args...>::const_iterator it = m.find(key);
-				if (it == m.end())
-					return std::numeric_limits<K>::infinity();
-				return it->second;
-			}
-
-			virtual std::list<Node*>
-			operator()(Node* start, Node* goal, size_t maxIterations = 1000)
-			{
-				std::set<Node*, CompareNode> closedSet;
-
-				std::map<Node*, Node*, CompareNode> cameFrom;
-
-				std::map<Node*, double, CompareNode> gScore;
-				gScore[start] = 0;
-
-				std::map<Node*, double, CompareNode> fScore;
-				fScore[start] = cost(start, goal);
-
-				std::set<CostNode> costSortedOpenSet;
-				costSortedOpenSet.emplace(start, GetWithDef(fScore, start));
-
-				for (; !costSortedOpenSet.empty() && maxIterations;
-					--maxIterations)
-				{
-					Node* current = costSortedOpenSet.begin()->node;
-
-					if (!current)
-						break;
-
-					if (current == goal)
-						return reconstructPath(cameFrom, current);
-
-					costSortedOpenSet
-						.erase(CostNode(current, GetWithDef(fScore, current)));
-					closedSet.insert(current);
-
-					for (size_t neighborIndex = 0;
-						neighborIndex < neighborCount(current); ++neighborIndex)
-					{
-						Node* neighbor = getNeighbor(current, neighborIndex);
-						if (closedSet.count(neighbor))
-							continue;
-
-						double tentativeScore = GetWithDef(gScore, current) +
-							cost(current, neighbor);
-						double tentativeCost  =
-								   tentativeScore + cost(neighbor, goal);
-
-						if (!costSortedOpenSet.count(
-							CostNode(neighbor, GetWithDef(fScore, neighbor))))
-							costSortedOpenSet.emplace(neighbor, tentativeCost);
-						else if (tentativeScore >= GetWithDef(gScore, neighbor))
-							continue;
-						else
-						{
-							costSortedOpenSet.erase(CostNode(neighbor,
-								GetWithDef(fScore, neighbor)));
-							costSortedOpenSet.emplace(neighbor, tentativeCost);
-						}
-
-						cameFrom[neighbor] = current;
-						gScore[neighbor]   = tentativeScore;
-						fScore[neighbor]   = tentativeCost;
-					}
-				}
-
-				std::list<Node*> empty;
-				return empty;
-			}
-
-			virtual std::list<Node*>
-			reconstructPath(std::map<Node*, Node*, CompareNode>& cameFrom,
-				Node* current)
-			{
-				std::list<Node*> path;
-				do
-				{
-					path.push_front(current);
-					current = cameFrom[current];
-				}
-				while (cameFrom.count(current));
-				path.push_front(current);
-				return path;
-			}
-
-			virtual double cost(Node* from, Node* to) = 0;
-
-			virtual Node*
-			getNeighbor(const Node* node, size_t neighborIndex) = 0;
-
-			virtual size_t neighborCount(const Node* node) = 0;
+	struct OrderByCost
+	{
+		bool operator()(const Node* node1, const Node* node2) const
+		{
+			if (node1->fScore == node2->fScore)
+				return *node1 < *node2;
+			else
+				return node1->fScore < node2->fScore;
+		}
 	};
+
+	struct SuperNode
+	{
+		double fScore;
+		double gScore;
+		Node* cameFrom;
+		bool closedSet;
+
+		SuperNode(double fScore = std::numeric_limits<double>::infinity(),
+			double gScore = std::numeric_limits<double>::infinity(),
+			Node* cameFrom = nullptr) :
+			fScore(fScore), gScore(gScore), cameFrom(cameFrom), closedSet(false)
+		{
+		}
+	};
+
+	virtual std::list<Node*>
+	operator()(Node* start, Node* goal, size_t maxIterations = 1000)
+	{
+		std::set<Node*, OrderByCost> openSet;
+		start->fScore = cost(start, goal);
+		start->gScore = 0;
+		openSet.insert(start);
+
+		for (; !openSet.empty() && maxIterations; --maxIterations)
+		{
+			Node* current = *openSet.begin();
+
+			if (!current)
+				break;
+
+			if (current == goal)
+				return reconstructPath(&*current);
+
+			openSet.erase(current);
+			current->closedSet = true;
+
+			for (size_t neighborIndex = 0;
+				neighborIndex < neighborCount(current); ++neighborIndex)
+			{
+				Node* neighbor = getNeighbor(current, neighborIndex);
+
+				if (neighbor->closedSet)
+					continue;
+
+				double
+					tentativeGScore = current->gScore + cost(current, neighbor);
+
+				if (neighbor->gScore <= tentativeGScore)
+					continue;
+
+				auto it = openSet.find(neighbor);
+
+				if (it != openSet.end())
+					openSet.erase(it);
+
+				neighbor->cameFrom = current;
+				neighbor->gScore   = tentativeGScore;
+				neighbor->fScore   = tentativeGScore + cost(neighbor, goal);
+				openSet.insert(neighbor);
+			}
+		}
+
+		std::list<Node*> empty;
+		return empty;
+	}
+
+	virtual std::list<Node*> reconstructPath(Node* current)
+	{
+		std::list<Node*> path;
+		while (current)
+		{
+			path.push_front(current);
+			current = current->cameFrom;
+		}
+		return path;
+	}
+
+	virtual double cost(Node* from, Node* to) = 0;
+
+	virtual Node* getNeighbor(const Node* node, size_t neighborIndex) = 0;
+
+	virtual size_t neighborCount(const Node* node) = 0;
+};
 }
 
 #endif //PROJECTS_ASTAR_HPP
