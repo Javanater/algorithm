aaeebb695e90f526f859ae938e06dab3144e5730
Madison Fortenberry <fortenberry.rm@gmail.com>
2016-11-21 21:11:08 -0500

    Minor AStar Performace Increase: No v-table look ups anywhere.

--- AStar.hpp
+++ AStar.hpp
@@ -6,22 +6,21 @@
 #define PROJECTS_ASTAR_HPP
 
 #include <list>
-#include <map>
-#include <queue>
 #include <set>
-#include <Utilities/utilities.h>
 
 namespace flabs
 {
-template<class Node>
+template<class Base, class Node>
 class AStar
 {
-public:
+protected:
+	Base* base;
 
-	virtual ~AStar()
+	AStar(Base* base) : base(base)
 	{
 	}
 
+public:
 	struct OrderByCost
 	{
 		bool operator()(const Node* node1, const Node* node2) const
@@ -40,19 +39,24 @@ public:
 		Node* cameFrom;
 		bool closedSet;
 
+	protected:
 		SuperNode(double fScore = std::numeric_limits<double>::infinity(),
 			double gScore = std::numeric_limits<double>::infinity(),
 			Node* cameFrom = nullptr) :
 			fScore(fScore), gScore(gScore), cameFrom(cameFrom), closedSet(false)
 		{
 		}
+
+		~SuperNode()
+		{
+		}
 	};
 
-	virtual std::list<Node*>
+	std::list<Node*>
 	operator()(Node* start, Node* goal, size_t maxIterations = 1000)
 	{
 		std::set<Node*, OrderByCost> openSet;
-		start->fScore = cost(start, goal);
+		start->fScore = base->cost(start, goal);
 		start->gScore = 0;
 		openSet.insert(start);
 
@@ -70,27 +74,24 @@ public:
 			current->closedSet = true;
 
 			for (size_t neighborIndex = 0;
-				neighborIndex < neighborCount(current); ++neighborIndex)
+				neighborIndex < base->neighborCount(current); ++neighborIndex)
 			{
-				Node* neighbor = getNeighbor(current, neighborIndex);
+				Node* neighbor = base->getNeighbor(current, neighborIndex);
 
 				if (neighbor->closedSet)
 					continue;
 
-				double
-					tentativeGScore = current->gScore + cost(current, neighbor);
+				double tentativeGScore = current->gScore +
+					base->cost(current, neighbor, neighborIndex);
 
 				if (neighbor->gScore <= tentativeGScore)
 					continue;
 
-				auto it = openSet.find(neighbor);
-
-				if (it != openSet.end())
-					openSet.erase(it);
-
+				openSet.erase(neighbor);
 				neighbor->cameFrom = current;
 				neighbor->gScore   = tentativeGScore;
-				neighbor->fScore   = tentativeGScore + cost(neighbor, goal);
+				neighbor->fScore   =
+					tentativeGScore + base->cost(neighbor, goal);
 				openSet.insert(neighbor);
 			}
 		}
@@ -99,7 +100,7 @@ public:
 		return empty;
 	}
 
-	virtual std::list<Node*> reconstructPath(Node* current)
+	std::list<Node*> reconstructPath(Node* current)
 	{
 		std::list<Node*> path;
 		while (current)
@@ -109,12 +110,6 @@ public:
 		}
 		return path;
 	}
-
-	virtual double cost(Node* from, Node* to) = 0;
-
-	virtual Node* getNeighbor(const Node* node, size_t neighborIndex) = 0;
-
-	virtual size_t neighborCount(const Node* node) = 0;
 };
 }
 
